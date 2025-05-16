09ff45165cfbb7a9cc1522d7e38d1835bdccb4b8
Prince-1562 <princemakani1562@gmail.com>
2025-05-16 11:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", function () {
+  const status = document.getElementById("status");
+  status.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+});
